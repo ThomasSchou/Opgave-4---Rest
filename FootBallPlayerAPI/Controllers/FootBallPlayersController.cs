29a71a3b9cb31afe91c1e5f73b6455f4dc988db6
ThomasSchou <thom63t4@edu.easj.dk>
2021-10-08 15:15:12 +0200

    Made Stuff

---
+++ FootBallPlayerAPI/Controllers/FootBallPlayersController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FootBallPlayerAPI.Items;
+using FootBallPlayerAPI.Managers;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+
+namespace FootBallPlayerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FootBallPlayersController : ControllerBase
+    {
+
+        public FootBallPlayerManager _manager = new FootBallPlayerManager();
+
+        [HttpGet()]
+        public List<FootBallPlayer> Get()
+        {
+            return _manager.GetAllPlayers();
+        }
+
+        [HttpGet("{id}")]
+        public FootBallPlayer Get(int id)
+        {
+            return _manager.GetPlayerByID(id);
+        }
+
+        [HttpPost]
+        public FootBallPlayer Post([FromBody] FootBallPlayer value)
+        {
+            return _manager.AddPlayer(value);
+        }
+
+        [HttpPut("{id}")]
+        public FootBallPlayer Put(int id, [FromBody] FootBallPlayer value)
+        {
+            return _manager.UpdateItem(id, value);
+        }
+
+        [HttpDelete("{id}")]
+        public FootBallPlayer Delete(int id)
+        {
+            return _manager.DeletePlayer(id);
+        }
+    }
+}
---
+++ FootBallPlayerAPI/Items/FootBallPlayer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FootBallPlayerAPI.Items
+{
+    public class FootBallPlayer
+    {
+        private int _id;
+        private string _name;
+        private int _price;
+        private int _shirtNumber;
+
+        public int ID
+        {
+            get => _id;
+            set => _id = value;
+        }
+
+        public string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+
+        public int Price
+        {
+            get => _price;
+            set => _price = value;
+        }
+
+        public int ShirtNumber
+        {
+            get => _shirtNumber;
+            set => _shirtNumber = value;
+        }
+    }
+}
